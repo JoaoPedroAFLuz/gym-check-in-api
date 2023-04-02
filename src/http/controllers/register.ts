fb871b41d413be9fa31fd1c1195e42ba60bc988c
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-04-01 23:22:09 -0300

    servico de cadastro de usuario

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,4 @@
-import { prisma } from '@/lib/prisma';
-import { hash } from 'bcryptjs';
+import { registerService } from '@/services/register';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
@@ -12,27 +11,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
-    return reply.status(409).send({
-      message: 'User already exists',
-    });
+  try {
+    await registerService({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send();
   }
-
-  const password_hash = await hash(password, 6);
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
